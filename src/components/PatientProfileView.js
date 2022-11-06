54b20e99106f2222d571a5e0b100e4794f402960
unknown <gkatrasnik@gmail.com>
2022-11-06 11:04:42 +0100

    implemented patient search

--- src/components/PatientProfileView.js
+++ src/components/PatientProfileView.js
@@ -6,18 +6,26 @@
 // current (open) issues (maybe show only those on bodyPicture?)
 
 import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { supabase } from '../supabase';
 import Layout from "./Layout";
 
 const PatientProfileView = () => {
    // const auth = useAuth();
    // const [userData, setUserData] = useState();
+    const location = useLocation();
 
+
+    useEffect(() => {
+    console.log("patient location state", location.state)
+    
+    }, [])
 
 
     return (
         <Layout>
             <h1>Patient Profile View</h1>
+            <div>{location.state.patientData.name}</div>
         </Layout>
     );
 };
--- src/components/PatientSearchView.js
+++ src/components/PatientSearchView.js
@@ -6,18 +6,26 @@ import { supabase } from '../supabase';
 import Layout from "./Layout";
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../auth';
+import { Form, Button,Col, Row, ListGroup, Container} from "react-bootstrap";
+
 
 const PatientSearchView = () => {
    const auth = useAuth();
-   const [patientsData, setPatientsData] = useState([]);
+   const [searchQuery, setSearchQuery] = useState("");
+   const [patientsData, setPatientsData] = useState([]);   
 
    const navigate = useNavigate();
+
+   const handleSubmit = (e) => {
+    e.preventDefault();
+    getPatients(auth.user.id, searchQuery);
+   }
 
    const getPatients = async (user_id, searchString) => {
         const queryData = await supabase
             .from('patients')
-            .select()
-            .eq("user_id", user_id)
+            .select("id, name")
+            .textSearch('name', searchString)
 
         if (queryData.error) {
             console.log(queryData.error.message);
@@ -26,28 +34,44 @@ const PatientSearchView = () => {
     }
 
    const toPatientProfile=(patient)=>{
-       navigate('/patient',{state:{patientData:patient}});
+    navigate('/patient',{state:{patientData:patient}});
    }
-
-   /*on search click {     
-    getPatients(auth.user.id, patientSearch);     
-   }, [])*/
    
 
 
 
     return (
         <Layout>
-            <>
-            <h1>Patient Search View</h1>
+            <Container>
+                <h1 className='text-center'>Patient Search View</h1>
 
-                {patientsData && patientsData.map((item, index) => {
-                    return (
-                        <h1>{item}</h1>
-                    )
-                })}
+                <Form className='my-3' onSubmit={handleSubmit}>
+                    <Row>
+                        <Col xs={8}>
+                            <Form.Group  controlId="patientSearch">
+                                <Form.Control type="text" placeholder="Search by name, phone, email..." 
+                                onChange={(e) => {
+                                setSearchQuery(e.target.value);
+                                }}/>                
+                            </Form.Group>
+                        </Col>
+                        <Col >
+                            <Button variant="primary" type="submit" >
+                                Submit
+                            </Button>
+                        </Col>
+                    </Row>            
+                </Form>
+                <ListGroup>
+                    {patientsData && patientsData.map((patient, index) => {
+                        return (
+                            <ListGroup.Item key={index} onClick={()=>{toPatientProfile(patient)}}> {patient.id}. {patient.name}</ListGroup.Item>
+                        )
+                    })}             
+                
+                </ListGroup>              
             
-            </>
+            </Container>
         </Layout>
     );
 };
