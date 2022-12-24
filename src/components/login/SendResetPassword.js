afc66904d5fd27183bd3cba2e1397778c7e36066
gkatrasnik <gkatrasnik@gmail.com>
2022-12-24 13:08:29 +0100

    added loading modal for password reset

--- src/components/login/SendResetPassword.js
+++ src/components/login/SendResetPassword.js
@@ -3,20 +3,25 @@ import { useNavigate } from "react-router-dom";
 import {useAuth} from "../../auth";
 import { Form, Button, Card, Container} from "react-bootstrap";
 import { Link } from "react-router-dom";
+import LoadingModal from "../modals/LoadingModal";
 
 const SendResetPassword = () => {
     const auth = useAuth();
     const [email, setEmail] = useState("");
     const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setLoading(true);
 
         const response = await auth.resetPassword(email);
 
         if(response.error) {
+            setLoading(false);
             alert(response.error.message);
         } else {
+          setLoading(false);
           alert("Reset password link was set to your email");
           setEmail("");
           navigate("/");
@@ -27,6 +32,7 @@ const SendResetPassword = () => {
 
     return (
     <Container className="min-h-100">
+      {loading && <LoadingModal />}
       <Card
         style={{ width: "90%", maxWidth: "32rem", margin: "auto", top: "10rem" }}
         className="box-shadow"
